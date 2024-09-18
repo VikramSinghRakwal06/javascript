10dc8827ead1a79100d6df80aaa5855c955aca51
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2024-09-18 21:33:33 +0000

    datatypes

---
+++ 01_BASICS/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let accountName = "Vikram";
+let age = 3;
+let state;
+let city = null;
+let loggedIn = true;
+
+console.table([accountName,age,state,city,loggedIn]);
+
+console.table([typeof accountName, typeof age, typeof state, typeof city, typeof loggedIn]);
+//typeof null = object
+//typeof undefined = undefined
