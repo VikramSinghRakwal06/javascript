032da9690bc28b1562aef12b850624d347b7e257
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2024-09-19 09:33:18 +0000

    datatypes2

---
+++ 01_BASICS/datatypes2.js
@@ -0,0 +1,14 @@
+let accountId = Symbol("123")
+let accId = Symbol("123");
+
+let myFun = function(){
+    console.log("Hello World");
+}
+let myObj = {
+    myName : "Vikram",
+    age : 22
+}
+
+let heroes = ["Shaktiman", "Naagraj", "Doga"];
+
+console.log(typeof myObj);
