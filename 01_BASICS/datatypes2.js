34881b0cbe268a646b0705bd54aa7a68e5cfeec9
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2024-09-19 15:04:22 +0530

    Update datatypes2.js

--- 01_BASICS/datatypes2.js
+++ 01_BASICS/datatypes2.js
@@ -1,14 +1,37 @@
-let accountId = Symbol("123")
-let accId = Symbol("123");
+//  Primitive
 
-let myFun = function(){
-    console.log("Hello World");
-}
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    myName : "Vikram",
-    age : 22
+    name: "hitesh",
+    age: 22,
 }
 
-let heroes = ["Shaktiman", "Naagraj", "Doga"];
+const myFunction = function(){
+    console.log("Hello world");
+}
 
-console.log(typeof myObj);
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
