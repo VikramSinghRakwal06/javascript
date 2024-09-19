da69fcc15c3f43156ef344939cfc4922b573c2af
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2024-09-19 20:22:20 +0000

    maths

---
+++ 01_BASICS/numsandmaths.js
@@ -0,0 +1,13 @@
+const num = 100;
+
+const num2 = new Number(num);
+
+console.log(num);
+console.log(num2);
+console.log(num2.toString());
+console.log(typeof num2);
+console.log(typeof num);
+console.log(num2.toFixed(2));
+
+const newNum = 122.636;
+console.log(newNum.toPrecision(3));
