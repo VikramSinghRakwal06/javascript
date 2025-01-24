607f7acbdd0a665c81a1d46fd35761d158d8b454
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-24 21:22:03 +0000

    url

---
+++ nodejs/moduleone.js
@@ -0,0 +1,4 @@
+//import { simple2 as simple } from "./modulesecond.mjs";
+import * as a from './modulesecond.mjs'
+a.simple()
+a.simple2();
---
+++ nodejs/modulesecond.mjs
@@ -0,0 +1,7 @@
+export  function simple(){
+    console.log('Node js is simple')
+}
+
+export function simple2(){
+    console.log('React is simple');
+}
