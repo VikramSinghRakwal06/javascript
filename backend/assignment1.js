ce283294da703552cdb825cd5248e123c9b18f0f
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-26 20:49:07 +0000

    assignment

---
+++ backend/assignment1.js
@@ -0,0 +1,19 @@
+ var prompt = require('prompt-sync')();
+//let age= prompt('Enter your age :');
+
+// if(age<18 && age>0){
+//     console.log('You get 20% discount on the tickets.')
+
+// }
+// else if(age>=18 && age < 65){
+//     console.log('You get tickets on normal price');
+// }
+// else{
+//     console.log('You get a 30% discount on ticket price');
+// }
+
+ let length = prompt('Enter the length of the rectangle: ');
+let width = prompt('Enter the width of a rectange : ');
+
+const area = length * width;
+console.log('The area of the rectangle is : '+ area);
