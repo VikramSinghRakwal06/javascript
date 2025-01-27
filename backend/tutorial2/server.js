b66642e92ed65c242b32cbedbb078d4bbdb407be
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-27 22:15:28 +0000

    ad

--- backend/tutorial2/server.js
+++ backend/tutorial2/server.js
@@ -1,6 +1,30 @@
-console.log('server is running');
-function add( a,  b){
-    return a+b;
+// console.log('server is running');
+// function add( a,  b){
+//     return a+b;
+// }
+// const ad = add(5,4);
+// console.log(ad)
+
+// function message(){
+//     console.log('Addition complete');
+// }
+
+// function add(a,b,message){
+//     const add = a+b;
+//     message();
+//     return add;
+// }
+
+// const ad = add(5,10,message);
+// console.log(ad);
+
+function add(a,b,message){
+    const res = a + b;
+   
+    console.log('result : '+res);
+    message(); 
 }
-const ad = add(5,4);
-console.log(ad)
+
+const result = add(5,4,()=>{
+    console.log('Who knows the event');
+})
