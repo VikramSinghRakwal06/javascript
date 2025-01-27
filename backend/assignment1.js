ae2abdecbaf5b5da26f15751bb1dbcc16c965cf8
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-27 09:06:21 +0000

    4

--- backend/assignment1.js
+++ backend/assignment1.js
@@ -19,19 +19,35 @@
 // console.log('The area of the rectangle is : '+ area);
 
 
-function product(name, price, inStock){
-    this.name = name;
-    this.price = price;
-    this.inStock= inStock;
+// function product(name, price, inStock){
+//     this.name = name;
+//     this.price = price;
+//     this.inStock= inStock;
+// }
+
+// const p1 = new product('maggi',10,true);
+
+// const p2 = new product(
+//     'coffee', 5, false
+// );
+
+// const p3 = new product('sugar', 20 , true);
+
+// const market = [p1,p2,p3];
+// console.log(market)
+
+const party = ['Vikram','Milan','Vanshaj','Akshat','Himneesh','Jalaj','Aayush','QT'];
+
+let guest = prompt('What is your name?');
+let invite = false;
+for(let i = 0;i<party.length;i++){
+    if(party[i]===guest){
+        console.log('You are invited to the party');
+        invite= true;
+    }
+    
 }
 
-const p1 = new product('maggi',10,true);
-
-const p2 = new product(
-    'coffee', 5, false
-);
-
-const p3 = new product('sugar', 20 , true);
-
-const market = [p1,p2,p3];
-console.log(market)
+if(!invite){
+    console.log('You are not invited to the party');
+}
