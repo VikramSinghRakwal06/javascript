8da1ffbcdf899de65cc8a9a60d389a5d01e9d60e
VikramSinghRakwal06 <vikramrakwal9682@gmail.com>
2025-01-27 22:31:06 +0000

    fs os

--- backend/tutorial2/server.js
+++ backend/tutorial2/server.js
@@ -18,13 +18,25 @@
 // const ad = add(5,10,message);
 // console.log(ad);
 
-function add(a,b,message){
-    const res = a + b;
+// function add(a,b,message){
+//     const res = a + b;
    
-    console.log('result : '+res);
-    message(); 
-}
+//     console.log('result : '+res);
+//     message(); 
+// }
 
-const result = add(5,4,()=>{
-    console.log('Who knows the event');
+// const result = add(5,4,()=>{
+//     console.log('Who knows the event');
+// })
+
+const os = require('os');
+const fs = require('fs');
+
+
+const user = os.userInfo();
+console.log(user);
+console.log(user.username);
+
+fs.appendFile('greeting.txt','Hi ' + user.username + '! \n',()=>{
+    console.log('file is created ')
 })
